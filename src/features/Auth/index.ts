25f1bc5836917fe84012ab66f658fa5db2bce368
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-06-16 20:37:55 +0300

    added backend for adding new friend

--- src/features/Auth/index.ts
+++ src/features/Auth/index.ts
@@ -1,4 +1,5 @@
+import { authConfig } from './lib/authConfig';
 import { LoginWithGoogle } from './ui/LoginWithGoogle';
 import { LogOut } from './ui/LogOut';
 
-export { LoginWithGoogle, LogOut };
+export { LoginWithGoogle, LogOut, authConfig };
